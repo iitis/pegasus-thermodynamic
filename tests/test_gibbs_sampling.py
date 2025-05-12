eabfe4de81ec0efeaa4a7cf0092ea97728e25f5b
tomsmierz <smirzchalski.tomek@gmail.com>
2025-05-12 15:56:30 +0200

    work in progress

--- tests/test_gibbs_sampling.py
+++ tests/test_gibbs_sampling.py
@@ -1,6 +1,8 @@
 import unittest
 import numpy as np
-from src.utils import gibbs_sampling_ising
+import dwave_networkx as dnx
+import time
+from src.utils import gibbs_sampling_ising_vectorized_2d
 from itertools import product
 from dimod import BinaryQuadraticModel
 from math import exp
@@ -10,26 +12,16 @@ rng = np.random.default_rng()
 
 
 class GibbsSampling(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        spins = 5
-        cls.distribution = {}
-        cls.beta = 1
-        cls.h = {i: rng.uniform(-1, 1) for i in range(spins)}
-        cls.J = {(i, i+1): rng.uniform(-1, 1) for i in range(spins-1)}
-        # bqm = BinaryQuadraticModel("SPIN")
-        # bqm = bqm.from_ising(cls.h, cls.J)
-        # for config in product([-1, 1], repeat=spins):
-        #     state = {idx: spin for idx, spin in enumerate(config)}
-        #     energy = bqm.energy(state)
-        #     cls.distribution[state] = exp(-cls.beta * energy)
-        # z = sum(list(cls.distribution.values()))
-        # for state, energy in cls.distribution.items():
-        #     cls.distribution[state] = energy/z
-
-    def test_gibbs_sampling(self):
-        sample = gibbs_sampling_ising(self.h, self.J, self.beta, 100)
+    ...
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    g = dnx.pegasus_graph(16, nice_coordinates=True)
+    g = dnx.pegasus_coordinates(16).nice_to_linear(g)
+    h = {node: np.random.uniform(-1, 1) for node in g.nodes()}
+    J = {edge: np.random.uniform(-1, 1) for edge in g.edges()}
+    start = time.time()
+    s = gibbs_sampling_ising_vectorized_2d(h, J, 1.0, 10000)
+    end = time.time()
+    print(end - start)
